ccee7c4a77c02584b17dcef19c4574a33b643c77
Fatheddine Bicane <fatheddinebicane@Fatheddines-MacBook-Air.local>
2024-11-12 13:34:35 +0100

    linked list 2

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,24 @@
 /*   By: fbicane <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:32:54 by fbicane           #+#    #+#             */
-/*   Updated: 2024/11/04 21:34:44 by fbicane          ###   ########.fr       */
+/*   Updated: 2024/11/11 19:10:33 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	ft_strlen_strlcat(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+	
+}
 
 size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 {
@@ -18,8 +31,10 @@ size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 	size_t	i;
 	size_t	j;
 
-	dest_len = ft_strlen(dest);
+	dest_len = ft_strlen_strlcat(dest);
 	i = dest_len;
+	if (dest_len >= destsize)
+		return (ft_strlen_strlcat(src));
 	j = 0;
 	while (src[j] != 0 && i < destsize - 1)
 	{
